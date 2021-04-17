2bf983b218d544c792c1ccb961b0e6e4da7153d1
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-04-17 10:57:25 +0700

    confused

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ ruby '2.7.2'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.3', '>= 6.1.3.1'
 # Use postgresql as the database for Active Record
-gem 'pg', '~> 1.1'
+# gem 'pg', '~> 1.1'
 # Use Puma as the app server
 gem 'puma', '~> 5.0'
 # Use SCSS for stylesheets
@@ -53,3 +53,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "devise", "~> 4.7"
+
+gem "mongoid", "~> 7.2"
--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+# Bundler.require(:default, Rails.env)
 
 module CovidFtsRails
   class Application < Rails::Application
@@ -18,5 +19,9 @@ module CovidFtsRails
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.generators do |g|
+      g.orm :active_record
+    end
   end
 end
